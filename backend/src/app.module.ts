8b28ec2bd73cfd039fddf57a610be53726013fbd
soyerv <valentin.soyer@epitech.eu>
2023-05-06 18:45:12 +0200

    feat(PostgreSQL): create the connection to the dockerized pg

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,42 @@
 import { Module } from '@nestjs/common';
 import { BaseModule } from './base/base.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [BaseModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        ENV_NAME: Joi.string().default('dev'),
+        APP_LOGGER: Joi.string().default('dev'),
+        APP_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_HOST: Joi.string().required(),
+      }),
+      isGlobal: true,
+      cache: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        autoLoadEntities: true,
+        // never use in production!
+        synchronize: configService.get('ENV_NAME') !== 'prod' ? true : false,
+      }),
+      inject: [ConfigService],
+    }),
+    BaseModule,
+  ],
   controllers: [],
   providers: [],
 })
