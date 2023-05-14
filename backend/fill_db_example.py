8b9c2f5f5ccbb3ee5f2169d5192404e0cce39a13
soyerv <valentin.soyer@epitech.eu>
2023-05-14 10:25:19 +0200

    refactor(DB auto fill): modify autofill script

--- backend/fill_db_example.py
+++ backend/fill_db_example.py
@@ -2,15 +2,28 @@ import requests
 import json
 import sys
 
+r = requests.post("http://127.0.0.1:8000/auth/signup", data={
+  "username": "admin",
+  "email": "admin@gmail.com",
+  "password": "root",
+  "passwordRepeat": "root",
+})
+
+print(r)
+
+r = requests.post("http://127.0.0.1:8000/auth/signin", data={
+  "email": "admin@gmail.com",
+  "password": "root",
+})
+
+token = json.loads(r.text)["accessToken"]
+print(r)
+
 with open(sys.argv[1], 'r') as f:
   data = json.load(f)
 
-print("Loading Activities: ", end="")
-for activity in data['activities']:
-  r = requests.post("http://127.0.0.1:8000/activities", data=activity)
-print("DONE")
-
-print("Loading Bases: ", end="")
+print("Loading Bases: ")
 for base in data['bases']:
-  r = requests.post("http://127.0.0.1:8000/bases", data=base)
-print("DONE")
+  r = requests.post("http://127.0.0.1:8000/bases", data=base, headers={'Authorization': 'Bearer ' + token})
+  print(r)
+print("DONE & SUCCESSFULL")
