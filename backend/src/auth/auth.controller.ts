8a39cff81703a5a12b51491a9926f051ab696ab9
soyerv <valentin.soyer@epitech.eu>
2023-05-14 13:50:23 +0200

    doc(swagger): add ApiProperties for Auth Module

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -2,20 +2,35 @@ import { Body, Controller, Logger, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
+import {
+  ApiTags,
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
 @Controller('auth')
+@ApiTags('Authentification')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   private logger = new Logger('Auth Controller');
 
   @Post('/signup')
+  @ApiCreatedResponse({ description: 'Successfull registration' })
+  @ApiBadRequestResponse({
+    description: "Your registration body doesn't match the expected body",
+  })
   signUp(@Body(ValidationPipe) registerDto: RegisterDto): Promise<void> {
     this.logger.verbose('[POST] / route is accessed');
     return this.authService.signUp(registerDto);
   }
 
   @Post('/signin')
+  @ApiCreatedResponse({ description: 'Generation of an access Token' })
+  @ApiUnauthorizedResponse({
+    description: 'Invalid credentials',
+  })
   signIn(
     @Body(ValidationPipe) loginDto: LoginDto,
   ): Promise<{ accessToken: string }> {
--- backend/src/auth/dto/register.dto.ts
+++ backend/src/auth/dto/register.dto.ts
@@ -1,20 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, IsEmail } from 'class-validator';
 
 export class RegisterDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   username: string;
 
   @IsString()
   @IsNotEmpty()
   @IsEmail()
+  @ApiProperty()
   email: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   password: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   passwordRepeat: string;
 }
